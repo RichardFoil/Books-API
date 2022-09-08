baddb0b6fa4c196315c5f3479b67e8b714982565
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-09-08 13:41:04 -0400

    install cors

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -40,6 +40,74 @@ books.get('/seed', (req, res) => {
         }))
 })
 
+// INDEX
+books.get('/', (req, res) => {
+  Book.find()
+      .then(foundBooks => {
+          res.status(200).json(foundBooks)
+      })
+      .catch(err => {
+          res.status(400).json({
+              message: 'An error has occurred, could not get all books'
+          })
+      })
+})
+
+// SHOW
+books.get('/:id', (req, res) => {
+  Book.findById(req.params.id)
+      .then(foundBook => {
+          res.status(200).json(foundBook)
+      })
+      .catch(err => {
+          res.status(400).json({
+              message: 'An error has occurred, could not find the book'
+          })
+      })
+})
+
+// UPDATE 
+books.put('/:id', (req, res) => {
+  Book.findByIdAndUpdate(req.params.id, req.body)
+      .then(updatedBook => {
+          console.log(req.body)
+          res.status(200).json(updatedBook)
+      })
+      .catch(err => {
+          res.status(400).json({
+              message: 'An error has occurred, could not edit the book'
+          })
+      })
+})
+
+// DELETE 
+books.delete('/:id', (req, res) => {
+  Book.findByIdAndDelete(req.params.id)
+      .then(deletedBook => {
+          res.status(200).json({
+              message: 'Delete successful'
+          })
+      })
+      .catch(err => {
+          res.status(400).json({
+              message: 'An error has occurred, could not delete the book'
+          })
+      })
+})
+
+// CREATE 
+books.post('/', (req, res) => {
+  Book.create(req.body)
+      .then(createdBook => {
+          res.status(200).json(createdBook)
+      })
+      .catch(err => {
+          res.status(400).json({
+              message: 'An error has occurred, could not create the book'
+          })
+      })
+})
+
 
 // EXPORT
 module.exports = books
